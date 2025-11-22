5af263d38a071af94c93960c2254394fba582c0e
szabop0126 <szabop1@edu.bme.hu>
2025-11-22 17:01:49 +0100

    usart tesztelese

---
+++ bszf_git_hazi/usart.c
@@ -0,0 +1,50 @@
+#include "em_usart.h"
+#include <stdbool.h>
+#include "em_cmu.h"
+#include "em_gpio.h"
+#include "stdio.h"
+
+volatile char usartValue = 'x';
+
+void usartInit(void){
+  CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_GPIO;
+  GPIO_PinModeSet(gpioPortF,7,gpioModePushPull,1);
+  CMU_ClockEnable(cmuClock_GPIO,true);
+  CMU_ClockEnable(cmuClock_UART0,true);
+
+
+  USART_InitAsync_TypeDef UART0_Init = USART_INITASYNC_DEFAULT;
+  USART_InitAsync(UART0, &UART0_Init);
+  GPIO_PinModeSet(gpioPortE,0,gpioModePushPull,1);
+  GPIO_PinModeSet(gpioPortE,1,gpioModeInput,0);
+  UART0->ROUTE |= UART_ROUTE_LOCATION_LOC1;
+  UART0->ROUTE |= UART_ROUTE_TXPEN | UART_ROUTE_RXPEN;
+
+  USART_IntClear(UART0,_USART_IFC_MASK);
+  USART_IntEnable(UART0,USART_IEN_RXDATAV);
+  NVIC_ClearPendingIRQ(UART0_RX_IRQn);
+  NVIC_EnableIRQ(UART0_RX_IRQn);
+}
+
+void GetChar(void){
+  usartValue = USART_RxDataGet(UART0);
+}
+
+void UART0_RX_IRQHandler(void){
+  uint32_t flags = USART_IntGet(UART0);
+
+  GetChar();
+
+  if(flags & USART_IF_RXDATAV){
+      USART_Tx(UART0,usartValue);
+  }
+
+
+  USART_IntClear(UART0,_USART_IFC_MASK);
+}
+
+
+
+
+
+
---
+++ bszf_git_hazi/usart.h
@@ -0,0 +1,8 @@
+#ifndef USART_H
+#define USART_H
+
+void usartInit(void);
+void UART0_RX_IRQHandler(void);
+void GetChar(void);
+
+#endif
